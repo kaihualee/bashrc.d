304573c1e73f0b7cdd459116f699c9091b8f058f
Lee Kaihua <leekaihua@192.168.0.102>
2015-06-07 23:07:33 +0800

    alter Mac ls for GNU Coreutils

--- darwin/alias.bash
+++ darwin/alias.bash
@@ -15,10 +15,6 @@ alias diff='colordiff'
 #12: Show open ports
 alias ports='netstat -tulanp'
 
-#16: Add safety nets
-# do not delete / or prompt if deleting more than 3 files at a time #
-alias rm='rm -I --preserve-root'
- 
 # confirmation #
 alias mv='mv -i'
 alias cp='cp -i'
--- darwin/ls.alias.bash
+++ darwin/ls.alias.bash
@@ -1,3 +1,9 @@
+if brew list | grep coreutils > /dev/null ; then
+  PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+  alias ls='ls -F --show-control-chars --color=auto'
+  eval `gdircolors -b $HOME/.bashrc.d/darwin/.dir_colors`
+fi
+
 ## Use a long listing format ##
 alias ll='ls -la'
  
