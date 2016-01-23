c17002b511e2a5e4193dcbe10a10779f7db6979e
kaihualee <kaihualee@outlook.com>
2016-01-23 20:54:17 +0800

    add tmux alias Force tmux to assume the terminal supports 256 colours.

--- linux/hadoop.alias.bash
+++ linux/hadoop.alias.bash
@@ -1,12 +1,3 @@
-#Hadoop Configuration
-
-base_dir=$(cd ~/blanc; pwd -P)
-conf_file="mulan"
-export JAVA_HOME="${base_dir}/parts/hadoop/hadoop-client/java6"
-chmod +x -R ${base_dir}
-alias hadoop="${base_dir}/parts/hadoop/hadoop-client/hadoop/bin/hadoop --config ${base_dir}/hadoop/${conf_file}_conf "
-alias hadoop_buildout_cmd="${base_dir}/build.sh hadoop"
-
 #Hadoop CMD alias
 alias hls='hadoop fs -ls '
 alias hcat='hadoop fs -cat '
